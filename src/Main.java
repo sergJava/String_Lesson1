caa03909eada6e4572f76f0e37196972d6d00f21
sergFibih <sergfibih@ya.ru>
2024-12-02 21:11:17 +1000

    урок 1 готов

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,22 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
 
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+
+        System.out.println("\nTask 2");
+        String fullNameForReport = fullName.toUpperCase();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета " + fullNameForReport);
+
+
+        System.out.println("\nTask 3");
+
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName.replace("ё", "е"));
     }
 }
